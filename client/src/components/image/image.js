9bafa2700a4baa8db9fb43066d5afedab38010b5
sohamkale <sohamkale@ufl.edu>
2020-04-30 07:13:49 -0400

    Responsive navbar

--- client/src/components/image/image.js
+++ client/src/components/image/image.js
@@ -2,11 +2,23 @@ import React, { useEffect, useState } from "react"
 import { Menu } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import image from "./mainimage.jpg";
+import image1 from "./backImage.jpg";
+import image2 from './backImage2.jpg';
 import "./image.css";
-
+import { useMediaPredicate } from "react-media-hook";
 const Image = () => {
+    const isMobile = useMediaPredicate("(max-width: 768px)");
+    // useEffect({
+    //     let a = useMediaPredicate("(max-width: 680px)");
+    //     setIsMobile(a);
+    // }, []);
     return(
-        <img className ="image"src = {image}/>
+        <div>
+            {isMobile ? <img className ="image" src = {image2}/> : <img className ="image"src = {image}/>}   
+            {/* <img className ="image"src = {image2}/> */}
+        </div>
+        
+       
     );
     
 
