cfec834aa0e827340891d3f385fce0d38abf2b67
sohamkale <sohamkale@ufl.edu>
2020-05-01 00:51:21 -0400

    projects added

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -7,17 +7,16 @@ import gif1 from "./gif1.gif";
 import "./Card.css";
 const Card = (props) => {
     return (
-    <div class="card xl-3 back bg-custom-1">
-    <div class="row no-gutters">
-      <div class="col-xl-3">
-        <img src={props.image} class="card-img" alt="..."/>
+    <div class="lg-3 border-0">
+    <div class="row no-gutters bg-custom-1">
+      <div class="col-lg-3 border-left border-top border-bottom border-3 flexDiv">
+        <img src={props.image} class="card-img imageframe" alt="..."/>
         {/* <iframe width="100%" height="100%" src="https://www.youtube.com/embed/GnvW1X5f3Zk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
       </div>
-      <br/>
-      <div class="col-xl-3">
-        <img src={props.image2}class="card-img" alt="..."/>
+      <div class="col-lg-3 col-md-borderflexDiv">
+        <img src={props.image2}class="card-img imageframe" alt="..."/>
       </div>
-      <div class="col-xl-6 bg-custom-1">
+      <div class="col-lg-6 border-0">
         <div class="card-body">
           <h5 class="card-title">{props.title}</h5>
           <p class="card-text">{props.desc}</p>
